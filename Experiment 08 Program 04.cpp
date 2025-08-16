796dbae9196b12342471b6b12b3639845a0d651e
Swarali Gurjar <gurjar.swarali@gmail.com>
2025-08-16 09:49:08 +0530

    Experiment 08 Program 04.cpp

---
+++ Experiment 08 Program 04.cpp
@@ -0,0 +1,26 @@
+//Code for diagonal addition
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the size of the square matrix: ";
+    cin >> n;
+
+    int matrix[10][10];
+    int sum = 0;
+    cout << "\nEnter the elements of the matrix:\n";
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> matrix[i][j];
+    for (int i = 0; i < n; i++) {
+        sum += matrix[i][i];             
+        sum += matrix[i][n - 1 - i];     
+    }
+    if (n % 2 == 1)
+        sum -= matrix[n / 2][n / 2];
+
+    cout << "\nSum of both diagonals = " << sum << endl;
+
+    return 0;
+}
